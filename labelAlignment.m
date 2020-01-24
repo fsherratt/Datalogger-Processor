556d271a6588d726d67240061a607417695c8cda
fsherratt <fsherratt@gmail.com>
2020-01-24 00:19:41 +0000

    Label aligned to HS

--- labelAlignment.m
+++ labelAlignment.m
@@ -1,51 +1,124 @@
-%% Load Data
-% Todo add in both right and left ankle
-% Todo adjust labels to neaerest HS
-
+% Load Data
 DataFolder = 'Data/Stair_Test/';
 
 [dataSet, labelSet] = loadFolder(DataFolder);
+labelSet = lableAlignment(dataSet, labelSet, true);
 
-% Plot gyro
-plot(dataSet.data(1).time, -dataSet.data(1).gyro(:, 3))
+%{
+% Function name - Short description
+% Long description
+%
+% Inputs: 
+%   var1 - description
+%
+% Outputs:
+%   rtn1 - description
+%
+% Other m-files required: 
+% Subfunctions: 
+% MAT-files required: 
+%
+% See also: fcn2
+%
+% Author: Freddie Sherratt
+% University of Bath
+% email: F.W.Sherratt@bath.ac.uk
+% Website: fsherratt.dev
+% Sep 2018; Last revision: 22-Jan-2020
+%}
+function [adjustedLabelSet] = lableAlignment(dataSet, labelSet, enableplot)
+    if nargin < 3
+        enableplot = false;
+    end
 
-%% Find heel strike
-[~, gyro_locs] = findpeaks(-dataSet.data(1).gyro(:, 3), ...
-                    'MinPeakProminence', 1, ...
-                    'MinPeakHeight', 100, ...
-                    'MinPeakDistance', 30 );
+    % Plot gyro
+    time = dataSet.data(1).time;
 
-[~, accel_locs] = findpeaks(vecnorm(dataSet.data(1).accel,2,2), ...
-                    'MinPeakProminence', 1, ...
-                    'MinPeakHeight', 20, ...
-                    'MinPeakDistance', 30 );
+    rAnkleRow = strcmp({dataSet.data.friendly}, 'Right Ankle');
+    lAnkleRow = strcmp({dataSet.data.friendly}, 'Left Ankle');
 
-% Each HS should be an accleration peak preceded by a leg swing
-hs = [];
-for i = 1:length(gyro_locs)
-    tmp = accel_locs( accel_locs > gyro_locs(i) );
-    
-    if ~isempty(tmp)
-        if ( tmp(1) - gyro_locs(i) ) < 100 % 1 second (100Hz) timeout
-            hs(end+1) = tmp(1);
+    aMagR = vecnorm(dataSet.data(rAnkleRow).accel,2,2);
+    gyroYR = -dataSet.data(rAnkleRow).gyro(:, 3);
+    aMagL = vecnorm(dataSet.data(lAnkleRow).accel,2,2);
+    gyroYL = dataSet.data(lAnkleRow).gyro(:, 3);
+
+    if enableplot
+        figure;
+        axes;
+        hold all
+        
+        plot(time, gyroYL)
+        plot(time, gyroYR)
+    end
+
+    % Find heel strike
+    hsR = identifyHeelStrike(aMagR, gyroYR);
+    hsL = identifyHeelStrike(aMagL, gyroYL);
+
+    if enableplot
+        plot(time(hsR), gyroYR(hsR), 'xr')
+        plot(time(hsL), gyroYL(hsL), 'or')
+    end
+
+    % Process labels
+    ix = zeros(length(labelSet.time), 1);
+    for i = 1:length(labelSet.time)
+    % Find index of label
+        [~, ix(i)] = min(abs(time - labelSet.time(i)));
+    end
+
+
+    % Plot label locations
+    if enableplot
+        for i = ix
+            plot([time(i), time(i)], [500, -500], 'g:');
+        end
+        ylim([-300, 400]);
+    end
+
+
+    % Adjust label locations - Align to closest heel strike (measured in time)
+    hs = [hsR, hsL];
+    ix_new = zeros(length(ix), 1);
+    for i = 1:length(ix)
+        [~, ix_new(i)] = min(abs(hs - ix(i)));
+    end
+    ix_new = hs(ix_new);
+
+    adjustedLabelSet = labelSet;
+    adjustedLabelSet.timeRow = ix_new;
+    adjustedLabelSet.time = time(ix_new);
+
+    % Plot new label locations
+    if enableplot
+        for i = ix_new
+            plot([time(i), time(i)], [500, -500], 'r:');
+        end
+    end
+    hold off
+end
+
+function [hs] = identifyHeelStrike(aMag, gyroY)
+    [~, gyro_locs] = findpeaks(gyroY, ...
+                        'MinPeakProminence', 1, ...
+                        'MinPeakHeight', 100, ...
+                        'MinPeakDistance', 30 );
+
+    [~, accel_locs] = findpeaks(aMag, ...
+                        'MinPeakProminence', 5, ...
+                        'MinPeakHeight', 12, ...
+                        'MinPeakDistance', 10 );
+
+    % Each HS should be an accleration peak preceded by a leg swing
+    hs = [];
+    for i = 1:length(gyro_locs)
+        tmp = accel_locs( accel_locs > gyro_locs(i) );
+
+        if ~isempty(tmp)
+            if ( tmp(1) - gyro_locs(i) ) < 100 % 1 second (100Hz) timeout
+                hs(end+1) = tmp(1);
+            end
         end
     end
 end
-
-%% Plot Heel strike
-hold all
-plot(dataSet.data(1).time(hs), -dataSet.data(1).gyro(hs, 3), 'x')
-hold off
-
-%% Process labels
-ix = zeros(length(labelSet.time), 1);
-for i = 1:length(labelSet.time)
-% Find index of label
-    [~, ix(i)] = min(abs(dataSet.data(1).time - labelSet.time(i)));
-end
-
-%% Plot label locations
-hold all
-plot(dataSet.data(1).time(ix), -dataSet.data(1).gyro(ix, 3), 'o')
-hold off
 
