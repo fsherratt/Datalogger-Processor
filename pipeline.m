e810a7f0d98b97d4466b332d95f9628c03c7ad81
Freddie Sherratt <fsherratt@gmail.com>
2020-01-28 12:09:07 +0000

    Added log file ouput

--- pipeline.m
+++ pipeline.m
@@ -1,10 +1,15 @@
+clear Config
 %---------------------------------------------------------------------------------------------------
 % Pipeline Configuration
 %---------------------------------------------------------------------------------------------------
 Config.DataFolder = 'Data/Stair_Test/'; % Folder containing the raw data
-Config.DataOutput = 'Out/'; % Folder to save output to
-Config.OutputPrefix = 'Out_';
-Config.OutputExtension = '.csv';
+
+Config.DataOutput = 'C:\Users\fs349\Desktop\Out\'; % Folder to save output to
+Config.OutputPrefix = 'Out_'; % Genearted file prefix
+Config.OutputSuffix = '.csv'; % Generated file suffix
+
+Config.OutputLogFilePath = Config.DataOutput; % Output log file save location
+Config.OutputLogFile = 'output.log'; % Output log file name
 
 Config.DataHeader = []; % Use default header file
 Config.TargetFrequency = 100; % Hz
@@ -41,15 +46,27 @@ for i = 1:length(dataFiles)
     fprintf('Generating output table\n');
     output = postProcessData(data, label);
        
+    % Generate file name
+    [~, file, ~]  = fileparts(dataFiles(i).data);
+    data_file = [Config.OutputPrefix, file, Config.OutputSuffix];
+    
     % Save to CSV
-    [~, data_file, ~]  = fileparts(dataFiles(i).data);
-    data_file = [Config.DataOutput, Config.OutputPrefix, data_file, Config.OutputExtension];
     fprintf('Saving to file: %s\n', reduceTextLength(data_file, 61));
-    writetable(output, data_file, 'FileType', 'text', 'Delimiter', ',');
+    writetable(output, [Config.DataOutput, data_file], 'FileType', 'text', 'Delimiter', ',');
+    
+    % Make log file
+    fprintf('Updating log file\n');
+    fid = fopen([Config.OutputLogFilePath, Config.OutputLogFile], 'a+');
+    date = string(datetime('now', 'TimeZone', 'local', 'Format', 'yyyy-MM-DD-HH-mm-SS'));
+    [~] = fprintf(fid, ['- %s:\n', '\tdate_created: %s\n', '\tsave_location: %s\n', ...
+                  '\tinput_file: %s\n', '\tsample_rate: %d\n', '\timu_calibration: %d\n'], ...
+                  data_file, date, Config.DataOutput, dataFiles(i).data, ...
+                  Config.TargetFrequency, Config.ApplyIMUCalibration);
+    [~] = fclose(fid);
     fprintf('--------------------------------------------------------------------------------\n');
 end
 
-clear dataFiles header i data label data_file
+clear dataFiles header i data label file data_file date fid
 
 %---------------------------------------------------------------------------------------------------
 % EOF
